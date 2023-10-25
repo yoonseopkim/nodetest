e85185d1be096a8c0e792426a32674481ba98719
jacob.oh <jacob.oh@goorm.io>
2023-10-25 18:10:46 +0900

    test2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"}); // (1)
-  response.write("Hello World");  // (2)
-  response.end();                 // (3)
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
 }).listen(80);
