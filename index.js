c1922ce2cb5211cc5ecc3cf2b87af9f400a4bee2
jacob.oh <jacob.oh@goorm.io>
2023-10-30 10:44:32 +0900

    test11

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 let express = require("express");
 let app = express();
 
-app.listen(3000, function(){
-    console.log("App is running on port 3000");
+app.listen(80, function(){
+    console.log("App is running on port 80");
 });
 
 app.get("/", function(req, res){
